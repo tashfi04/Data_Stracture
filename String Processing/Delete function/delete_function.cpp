a0b919fec8d7532b42d95ccf694409acc30cae31
Tashfi <43473062+tashfi04@users.noreply.github.com>
2018-09-26 11:43:28 +0600

    Replace function

---
+++ String Processing/Delete function/delete_function.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void del(string *text, int pos, int length)
+{
+    string first_part,second_part;
+    int x;
+    int len = (*text).size();
+
+    for(int i=0; i<pos-1; i++)
+        first_part = first_part+(*text)[i];
+
+    x=pos-1+length;
+
+    for(int i=x; i<len; i++)
+        second_part = second_part + (*text)[i];
+
+    (*text)=first_part+second_part;
+}
+
+int main()
+{
+    int pos,length;
+    string text;
+
+    cout<<"Enter the Text:"<<endl;
+    getline(cin,text);
+
+    cout<<"Enter the position:"<<endl;
+    cin>>pos;
+
+    cout<<"Enter the length:"<<endl;
+    cin>>length;
+
+    del(&text,pos,length);
+    cout<<text<<endl;
+}
